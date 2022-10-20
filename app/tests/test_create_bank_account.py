3e72d3aaaec67adf96687f420475e316ae31b6a3
Grzegorz Rogoziński <g.rogozinski.649@studms.ug.edu.pl>
2022-10-20 17:12:03 +0200

    Feature 3

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -8,10 +8,11 @@ class TestCreateBankAccount(unittest.TestCase):
         imie = "Dariusz"
         nazwisko = "Januszewski"
         pesel = "97012663712"
-        pierwsze_konto = Konto(imie, nazwisko)
+        pierwsze_konto = Konto(imie, nazwisko, pesel)
         self.assertEqual(pierwsze_konto.imie, imie, "Imie nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.nazwisko, nazwisko, "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.pesel, pesel, "PESEL nie został zapisany!")
+        self.assertEqual(len(pierwsze_konto.pesel), 11, "PESEL ma złą długość!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
 
     #tutaj proszę dodawać nowe testy
