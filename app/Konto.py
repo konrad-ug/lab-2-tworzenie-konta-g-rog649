71a43cfa66a618d9be737eac6e170e4a57eb32e1
Grzegorz Rogoziński <g.rogozinski.649@studms.ug.edu.pl>
2022-10-20 17:09:33 +0200

    Feature 2

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,7 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, pesel):
         self.imie = imie
         self.nazwisko = nazwisko
+        self.pesel = pesel
+
         self.saldo = 0
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -5,9 +5,13 @@ from ..Konto import Konto
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        imie = "Dariusz"
+        nazwisko = "Januszewski"
+        pesel = "97012663712"
+        pierwsze_konto = Konto(imie, nazwisko)
+        self.assertEqual(pierwsze_konto.imie, imie, "Imie nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.nazwisko, nazwisko, "Nazwisko nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.pesel, pesel, "PESEL nie został zapisany!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
 
     #tutaj proszę dodawać nowe testy
