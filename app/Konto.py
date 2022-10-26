5e843a7ac0ec65bfb2758ed454eb22ee823d5e22
g-rog649 <g.rogozinski.649@studms.ug.edu.pl>
2022-10-26 14:48:24 +0200

    Feature 4

--- app/Konto.py
+++ app/Konto.py
@@ -2,7 +2,17 @@ class Konto:
     def __init__(self, imie, nazwisko, pesel, rabat=None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.saldo = 0
+
+        rabat_start = "PROM_"
+        rabat_chars = 3
+        if (
+            rabat is not None
+            and rabat.startswith(rabat_start)
+            and len(rabat) == len(rabat_start) + rabat_chars
+        ):
+            self.saldo = 50
+        else:
+            self.saldo = 0
 
         if len(pesel) == 11:
             self.pesel = pesel
