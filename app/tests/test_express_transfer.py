626938c9e8a950beb29819cdc215d301639e7531
g-rog.649 <g.rogozinski.649@studms.ug.edu.pl>
2022-11-09 20:50:14 +0100

    Fix transfer function arguments

--- app/tests/test_express_transfer.py
+++ app/tests/test_express_transfer.py
@@ -13,31 +13,31 @@ class TestExpressTransfer(unittest.TestCase):
 
         cls.nazwa_firmy = "Januszex sp. z o.o."
         cls.nip = "8461627563"
-    
 
     def test_udany_przelew_ekspresowy(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.saldo = 500
-        konto.zaksieguj_przelew(400, rodzaj="ekspresowy")
-        self.assertEqual(konto.saldo, 99, "Saldo się nie zgadza!")
+        konto1 = Konto(self.imie, self.nazwisko, self.pesel)
+        konto2 = Konto(self.imie, self.nazwisko, self.pesel)
+        konto1.saldo = 500
+        konto1.zaksieguj_przelew(400, konto2, rodzaj="ekspresowy")
+        self.assertEqual(konto1.saldo, 99, "Saldo się nie zgadza!")
 
-    
     def test_nieudany_przelew_ekspresowy(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.saldo = 500
-        konto.zaksieguj_przelew(600, rodzaj="ekspresowy")
-        self.assertEqual(konto.saldo, 500, "Saldo się zmieniło!")
-
+        konto1 = Konto(self.imie, self.nazwisko, self.pesel)
+        konto2 = Konto(self.imie, self.nazwisko, self.pesel)
+        konto1.saldo = 500
+        konto1.zaksieguj_przelew(600, konto2, rodzaj="ekspresowy")
+        self.assertEqual(konto1.saldo, 500, "Saldo się zmieniło!")
 
     def test_udany_przelew_ekspresowy_konto_firmowe(self):
-        konto = KontoFirmowe(self.nazwa_firmy, self.nip)
-        konto.saldo = 500
-        konto.zaksieguj_przelew(400, rodzaj="ekspresowy")
-        self.assertEqual(konto.saldo, 95, "Saldo nie jest poprawne!")
-
+        konto1 = KontoFirmowe(self.nazwa_firmy, self.nip)
+        konto2 = KontoFirmowe(self.nazwa_firmy, self.nip)
+        konto1.saldo = 500
+        konto1.zaksieguj_przelew(400, konto2, rodzaj="ekspresowy")
+        self.assertEqual(konto1.saldo, 95, "Saldo nie jest poprawne!")
 
     def test_udany_przelew_ekspresowy_saldo_ponizej_0(self):
-        konto = KontoFirmowe(self.nazwa_firmy, self.nip)
-        konto.saldo = 500
-        konto.zaksieguj_przelew(600, rodzaj="ekspresowy")
-        self.assertEqual(konto.saldo, 500, "Saldo nie jest poprawne!")
+        konto1 = KontoFirmowe(self.nazwa_firmy, self.nip)
+        konto2 = KontoFirmowe(self.nazwa_firmy, self.nip)
+        konto1.saldo = 500
+        konto1.zaksieguj_przelew(600, konto2, rodzaj="ekspresowy")
+        self.assertEqual(konto1.saldo, 500, "Saldo nie jest poprawne!")
