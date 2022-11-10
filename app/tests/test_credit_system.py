8b60d73800646a60b63a038fd8338122ed38f29f
Grzegorz Rogoziński <g.rogozinski.649@studms.ug.edu.pl>
2022-11-10 16:32:16 +0100

    Feature 12 - tests

---
+++ app/tests/test_credit_system.py
@@ -0,0 +1,30 @@
+import unittest
+
+from ..Konto import Konto
+
+
+class TestCreditSystem(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.imie = "Dariusz"
+        cls.nazwisko = "Januszewski"
+        cls.pesel = "87013092831"
+
+    def test_kredyt_nie_udzielony(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel)
+        zaciagniety = konto.zaciagnij_kredyt(500)
+        self.assertFalse(zaciagniety, "Kredyt został zaciągnięty, a nie powinien być!")
+    
+    def test_kredyt_udzielony_wplaty(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel)
+        konto.historia = [-100, 100, 100, 100]
+        zaciagniety = konto.zaciagnij_kredyt(500)
+        self.assertEqual(konto.saldo, 500, "Saldo się nie zgadza!")
+        self.assertTrue(zaciagniety, "Kredyt nie został zaciągnięty, a powinien być!")
+    
+    def test_kredyt_udzielony_transakcje(self):
+        konto = Konto(self.imie, self.nazwisko, self.pesel)
+        konto.historia = [-1000, 100, -200, 100, 700, -200]
+        zaciagniety = konto.zaciagnij_kredyt(500)
+        self.assertEqual(konto.saldo, 500, "Saldo się nie zgadza!")
+        self.assertTrue(zaciagniety, "Kredyt nie został zaciągnięty, a powinien być!")
