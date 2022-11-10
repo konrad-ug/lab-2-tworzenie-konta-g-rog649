f33795c3a6017eb820e1b0e660a031931c4d8afb
Grzegorz Rogoziński <g.rogozinski.649@studms.ug.edu.pl>
2022-11-10 17:27:16 +0100

    Feature 12 - refactor, parameterization

--- app/tests/test_credit_system.py
+++ app/tests/test_credit_system.py
@@ -1,31 +1,43 @@
 import unittest
+from parameterized import parameterized
 
 from ..Konto import Konto
 
 
 class TestCreditSystem(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.imie = "Dariusz"
-        cls.nazwisko = "Januszewski"
-        cls.pesel = "87013092831"
-
-    def test_kredyt_nie_udzielony(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.historia = [-100, 100, -200, 100, 100]
-        zaciagniety = konto.zaciagnij_kredyt(500)
+    def setUp(self):
+        imie = "Dariusz"
+        nazwisko = "Januszewski"
+        pesel = "87013092831"
+        self.konto = Konto(imie, nazwisko, pesel)
+    
+    @parameterized.expand([
+        [[-100, 100, -200, 100, 100]],
+        [[]]
+    ])
+    def test_kredyt_nie_udzielony(self, historia):
+        self.konto.historia = historia
+        zaciagniety = self.konto.zaciagnij_kredyt(500)
         self.assertFalse(zaciagniety, "Kredyt został zaciągnięty, a nie powinien być!")
     
-    def test_kredyt_udzielony_wplaty(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.historia = [-100, 100, 100, 100]
-        zaciagniety = konto.zaciagnij_kredyt(500)
-        self.assertEqual(konto.saldo, 500, "Saldo się nie zgadza!")
+    @parameterized.expand([
+        [[-100, 100, 100, 100]],
+        [[-200, -300, 200, 100, 50]],
+        [[-500, -400, -600, 300, 1000, 500]],
+    ])
+    def test_kredyt_udzielony_wplaty(self, historia):
+        self.konto.historia = historia
+        zaciagniety = self.konto.zaciagnij_kredyt(500)
+        self.assertEqual(self.konto.saldo, 500, "Saldo się nie zgadza!")
         self.assertTrue(zaciagniety, "Kredyt nie został zaciągnięty, a powinien być!")
     
-    def test_kredyt_udzielony_transakcje(self):
-        konto = Konto(self.imie, self.nazwisko, self.pesel)
-        konto.historia = [-1000, 100, -200, 100, 800, -200]
-        zaciagniety = konto.zaciagnij_kredyt(500)
-        self.assertEqual(konto.saldo, 500, "Saldo się nie zgadza!")
+    @parameterized.expand([
+        [[-1000, 100, -200, 100, 800, -200]],
+        [[-300, 200, 300, -100, 300, -100, 200]],
+        [[-300, 200, -1000, -100, 300, 200, 400, -200]],
+    ])
+    def test_kredyt_udzielony_transakcje(self, historia):
+        self.konto.historia = historia
+        zaciagniety = self.konto.zaciagnij_kredyt(500)
+        self.assertEqual(self.konto.saldo, 500, "Saldo się nie zgadza!")
         self.assertTrue(zaciagniety, "Kredyt nie został zaciągnięty, a powinien być!")
