a49fbfaa40f5c7fd64b7996580aa541e307b6658
Grzegorz Rogoziński <g.rogozinski.649@studms.ug.edu.pl>
2022-11-17 16:47:41 +0100

    Feature 13 - tests

--- app/tests/test_credit_system.py
+++ app/tests/test_credit_system.py
@@ -2,7 +2,7 @@ import unittest
 from parameterized import parameterized
 
 from ..Konto import Konto
-
+from ..KontoFirmowe import KontoFirmowe
 
 class TestCreditSystem(unittest.TestCase):
     def setUp(self):
@@ -10,6 +10,15 @@ class TestCreditSystem(unittest.TestCase):
         nazwisko = "Januszewski"
         pesel = "87013092831"
         self.konto = Konto(imie, nazwisko, pesel)
+
+        nazwa_firmy = "Januszex sp. z o.o."
+        nip = "8461627563"
+        
+        self.kontoFirmowe = KontoFirmowe(nazwa_firmy, nip)
+        self.messages = [
+            "Kredyt został zaciągnięty, a nie powinien być!",
+            "Kredyt nie został zaciągnięty, a powinien być!"
+        ]
     
     @parameterized.expand([
         # Nie udzielenie
@@ -24,11 +33,24 @@ class TestCreditSystem(unittest.TestCase):
         [[-300, 200, -1000, -100, 300, 200, 400, -200], 350, True],
     ])
     def test_kredyt(self, historia, kwota, udany):
-        messages = [
-            "Kredyt został zaciągnięty, a nie powinien być!",
-            "Kredyt nie został zaciągnięty, a powinien być!"
-        ]
         self.konto.historia = historia
         zaciagniety = self.konto.zaciagnij_kredyt(kwota)
-        self.assertEqual(zaciagniety, udany, messages[udany])
+        self.assertEqual(zaciagniety, udany, self.messages[udany])
 
+
+    @parameterized.expand([
+        # Saldo nie 2 razy większe, niż zaciągnięta kwota
+        [[1775], 1000, 1000, False],
+        [[1775], 1000, 2000, False],
+        # W historii brak przelewu do ZUS
+        [[], 1000, 2001, False],
+        [[1324], 1000, 2001, False],
+        # Oba warunki spełnone
+        [[1775], 1000, 2001, True],
+        [[-300, 1775, -500], 1000, 2001, True]
+    ])
+    def test_kredyt_konto_firmowe(self, historia, saldo, kwota, udany):
+        self.kontoFirmowe.historia = historia
+        self.kontoFirmowe.saldo = saldo
+        zaciagniety = self.kontoFirmowe.zaciagnij_kredyt(kwota)
+        self.assertEqual(zaciagniety, udany, self.messages[udany])
