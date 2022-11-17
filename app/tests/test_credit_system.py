ceae9c4c70c8b64d8f1cd8f81234e4aac2633e44
Grzegorz Rogoziński <g.rogozinski.649@studms.ug.edu.pl>
2022-11-17 16:29:17 +0100

    Feature 12 refactor

--- app/tests/test_credit_system.py
+++ app/tests/test_credit_system.py
@@ -12,32 +12,23 @@ class TestCreditSystem(unittest.TestCase):
         self.konto = Konto(imie, nazwisko, pesel)
     
     @parameterized.expand([
-        [[-100, 100, -200, 100, 100]],
-        [[]]
+        # Nie udzielenie
+        [[-100, 100, -200, 100, 100], 500, False],
+        # Wpłata - trzy ostatnie dodatnie
+        [[-100, 100, 100, 100], 500, True],
+        [[-200, -300, 200, 100, 50], 600, True],
+        [[-500, -400, -600, 300, 1000, 500], 700, True],
+        # Wpłata - suma 5 większa, niż kwota
+        [[-1000, 100, -200, 100, 800, -200], 550, True],
+        [[-300, 200, 300, -100, 300, -100, 200], 100, True],
+        [[-300, 200, -1000, -100, 300, 200, 400, -200], 350, True],
     ])
-    def test_kredyt_nie_udzielony(self, historia):
+    def test_kredyt(self, historia, kwota, udany):
+        messages = [
+            "Kredyt został zaciągnięty, a nie powinien być!",
+            "Kredyt nie został zaciągnięty, a powinien być!"
+        ]
         self.konto.historia = historia
-        zaciagniety = self.konto.zaciagnij_kredyt(500)
-        self.assertFalse(zaciagniety, "Kredyt został zaciągnięty, a nie powinien być!")
-    
-    @parameterized.expand([
-        [[-100, 100, 100, 100]],
-        [[-200, -300, 200, 100, 50]],
-        [[-500, -400, -600, 300, 1000, 500]],
-    ])
-    def test_kredyt_udzielony_wplaty(self, historia):
-        self.konto.historia = historia
-        zaciagniety = self.konto.zaciagnij_kredyt(500)
-        self.assertEqual(self.konto.saldo, 500, "Saldo się nie zgadza!")
-        self.assertTrue(zaciagniety, "Kredyt nie został zaciągnięty, a powinien być!")
-    
-    @parameterized.expand([
-        [[-1000, 100, -200, 100, 800, -200]],
-        [[-300, 200, 300, -100, 300, -100, 200]],
-        [[-300, 200, -1000, -100, 300, 200, 400, -200]],
-    ])
-    def test_kredyt_udzielony_transakcje(self, historia):
-        self.konto.historia = historia
-        zaciagniety = self.konto.zaciagnij_kredyt(500)
-        self.assertEqual(self.konto.saldo, 500, "Saldo się nie zgadza!")
-        self.assertTrue(zaciagniety, "Kredyt nie został zaciągnięty, a powinien być!")
+        zaciagniety = self.konto.zaciagnij_kredyt(kwota)
+        self.assertEqual(zaciagniety, udany, messages[udany])
+
