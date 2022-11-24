095b100f41c8c717f4db2ef2ad63e2b6bdfb3b7e
Grzegorz Rogoziński <g.rogozinski.649@studms.ug.edu.pl>
2022-11-24 18:20:15 +0100

    Feature 14 - fix tests

--- app/tests/test_account_register.py
+++ app/tests/test_account_register.py
@@ -1,6 +1,7 @@
 import unittest
 
-from ../RejestrKont import RejestrKont
+from ..Konto import Konto
+from ..RejestrKont import RejestrKont
 
 class TestAccountRegister(unittest.TestCase):
     def setUp(self):
@@ -12,22 +13,27 @@ class TestAccountRegister(unittest.TestCase):
         imie2 = "Dariusz"
         nazwisko2 = "Januszewski"
         pesel2 = "94120294917"
-        self.konto2 = Konto(imie, nazwisko, pesel)
+        self.konto2 = Konto(imie2, nazwisko2, pesel2)
     
     def test_ile_kont(self):
+        RejestrKont.konta = []
         self.assertEqual(RejestrKont.ile_kont(), 0)
+
         RejestrKont.dodaj_konto(self.konto1)
         self.assertEqual(RejestrKont.ile_kont(), 1)
+
         RejestrKont.dodaj_konto(self.konto2)
         self.assertEqual(RejestrKont.ile_kont(), 2)
     
     def test_dodaj_konto(self):
+        RejestrKont.konta = []
         RejestrKont.dodaj_konto(self.konto1)
         self.assertListEqual(RejestrKont.konta, [self.konto1], "Konto nie zostało dodane!")
 
     def test_wyszukaj_konto(self):
+        RejestrKont.konta = []
         RejestrKont.dodaj_konto(self.konto1)
         RejestrKont.dodaj_konto(self.konto2)
-        found_account = RejestrKont.wyszukaj_konto(pesel2)
-        self.assertEqual(found_account, konto2, "Złe konto znalezione!")
+        found_account = RejestrKont.wyszukaj_konto(self.konto2.pesel)
+        self.assertEqual(found_account, self.konto2, "Złe konto znalezione!")
     
