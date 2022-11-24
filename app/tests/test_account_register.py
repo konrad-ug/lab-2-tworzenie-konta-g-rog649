076acf08cfdce5f41ff777b35bdb5907c9fb91df
Grzegorz Rogoziński <g.rogozinski.649@studms.ug.edu.pl>
2022-11-24 18:27:46 +0100

    Feature 14 - refactor

--- app/tests/test_account_register.py
+++ app/tests/test_account_register.py
@@ -4,19 +4,26 @@ from ..Konto import Konto
 from ..RejestrKont import RejestrKont
 
 class TestAccountRegister(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         imie1 = "Dariusz"
         nazwisko1 = "Januszewski"
         pesel1 = "87013092831"
-        self.konto1 = Konto(imie1, nazwisko1, pesel1)
+        cls.konto1 = Konto(imie1, nazwisko1, pesel1)
 
         imie2 = "Dariusz"
         nazwisko2 = "Januszewski"
         pesel2 = "94120294917"
-        self.konto2 = Konto(imie2, nazwisko2, pesel2)
+        cls.konto2 = Konto(imie2, nazwisko2, pesel2)
+    
+    def tearDown(self):
+        RejestrKont.konta = []
+    
+    @classmethod
+    def tearDownClass(cls):
+        RejestrKont.konta = []
     
     def test_ile_kont(self):
-        RejestrKont.konta = []
         self.assertEqual(RejestrKont.ile_kont(), 0)
 
         RejestrKont.dodaj_konto(self.konto1)
@@ -26,12 +33,10 @@ class TestAccountRegister(unittest.TestCase):
         self.assertEqual(RejestrKont.ile_kont(), 2)
     
     def test_dodaj_konto(self):
-        RejestrKont.konta = []
         RejestrKont.dodaj_konto(self.konto1)
         self.assertListEqual(RejestrKont.konta, [self.konto1], "Konto nie zostało dodane!")
 
     def test_wyszukaj_konto(self):
-        RejestrKont.konta = []
         RejestrKont.dodaj_konto(self.konto1)
         RejestrKont.dodaj_konto(self.konto2)
         found_account = RejestrKont.wyszukaj_konto(self.konto2.pesel)
