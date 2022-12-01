7bff602d4cf1529762eb2dd0f2d1ecd80650000f
Grzegorz Rogoziński <g.rogozinski.649@studms.ug.edu.pl>
2022-12-01 19:11:40 +0100

    API Tests

---
+++ app/tests/api/test_api_account.py
@@ -0,0 +1,24 @@
+import unittest
+import requests
+
+class TestCreateAccount(unittest.TestCase):
+    body = {
+        "imie": "Dariusz",
+        "nazwisko": "Januszewski",
+        "pesel": "87013092831"
+    }
+
+    url = "localhost:5000"
+
+    def test_create_account_correct(self):
+        response = requests.post(self.url + "/konta/stworz_konto", json=self.body)
+        self.assertEqual(response.status_code, 201, "Zły response!")
+    
+    def test_get_account_pesel(self):
+        response = requests.get(self.url + f"/konta/konto/{self.body['pesel']}")
+        self.assertEqual(response.status_code, 200, "Zły response!")
+
+        response_body = response.json()
+        self.assertEqual(response_body["imie"], self.body["imie"], "Imię inne!")
+        self.assertEqual(response_body["nazwisko"], self.body["nazwisko"], "Nazwisko inne!")
+        self.assertEqual(response_body["pesel"], self.body["pesel"], "PESEL inne!")
